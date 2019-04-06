9ac15398cf0bad5673ff3caee6479292e47187fd
BogdanVidrean <bogdan.vidrean@yahoo.com>
2019-04-06 11:05:48 +0300

    Added some basic styling for main page in voter's view

--- core/src/main/resources/solidity/Election.sol
+++ core/src/main/resources/solidity/Election.sol
@@ -13,6 +13,14 @@ contract Election {
     
     constructor() public {
         voters[0x8976fC040A9F2B22974E598387f40fF8e1125E7b] = 1;
+        voters[0xCA56BdFF4aB30b96B4F618e718B88C7a1721ECC3] = 1;
+        voters[0xA19b33F09A8E09d6f7D9330115038a87fe331878] = 1;
+        voters[0x480AC6bC8E492D7840c5EF362Dc90Ff82A704Ad1] = 1;
+        voters[0x32E33d20d2a5d0D05F7C088AAaB8f3e7A7F5F4fD] = 1;
+        voters[0x3ae77c608E84e349a4312Da6Bb7bCb413755765b] = 1;
+        voters[0x52B4BF9844edf3956d6E860cF92bC2CD224164CC] = 1;
+        voters[0xf06bc4Dd7BC1738a24c473859C391Bcf81c0f0aE] = 1;
+
         
         candidates[0] = Candidate(0, "Gigel", true);
         candidates[1] = Candidate(1, "Dorel", true);
--- voter/src/main/java/com/blockvote/voter/controllers/MainPageController.java
+++ voter/src/main/java/com/blockvote/voter/controllers/MainPageController.java
@@ -1,17 +1,27 @@
 package com.blockvote.voter.controllers;
 
 import com.blockvote.core.contracts.Election;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.DefaultGasProvider;
 
+import java.util.List;
+
 import static com.blockvote.core.os.Commons.CONTRACT_ADDRESS;
+import static java.util.stream.Collectors.toList;
+import static javafx.collections.FXCollections.observableArrayList;
 
 public class MainPageController {
 
-    Credentials credentials;
+    @FXML
+    private ListView candidatesList;
+
+    private ObservableList<String> candidatesObsList = observableArrayList();
+    private Credentials credentials;
 
     @FXML
     public void initialize() {
@@ -21,7 +31,14 @@ public class MainPageController {
     public void setCredentials(Credentials credentials) {
         this.credentials = credentials;
         Web3j web3j = Web3j.build(new HttpService("http://localhost:8507"));
-        Election load = Election.load(CONTRACT_ADDRESS, web3j, credentials, new DefaultGasProvider());
+        Election election = Election.load(CONTRACT_ADDRESS, web3j, credentials, new DefaultGasProvider());
+        try {
+            List<byte[]> candidates = election.getCandidates().send();
+            candidatesObsList.addAll(candidates.stream().map(String::new).collect(toList()));
+            candidatesList.setItems(candidatesObsList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
